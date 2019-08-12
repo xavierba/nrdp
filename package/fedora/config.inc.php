xavierba/nrdp

commit a675f42955e21dad33b1389bfd3dca4a852ecf07

* changed license to gpl * rewrote a lot of code for clarity

package/fedora/config.inc.php
@@ -1,76 +1,169 @@
 <?php
-//
-// NRDP Config File
-//
-// Copyright (c) 2010-2017 - Nagios Enterprises, LLC.
-// License: Nagios Open Software License <http://www.nagios.com/legal/licenses>
-//
+/*****************************************************************************
+ *
+ *
+ *  NRDP Configuration file
+ *
+ *
+ *  Copyright (c) 2008-2018 - Nagios Enterprises, LLC. All rights reserved.
+ *
+ *  License: GNU General Public License version 3
+ *
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ *****************************************************************************/
 
+
+
+/////////////////////////////////////////////////////////////
+//
+// authorized_tokens
+//
 // An array of one or more tokens that are valid for this NRDP install
 // a client request must contain a valid token in order for the NRDP to response or honor the request
 // NOTE: Tokens are just alphanumeric strings - make them hard to guess!
-$cfg['authorized_tokens'] = array(
-    //"mysecrettoken",  // <-- not a good token
-    //"90dfs7jwn3",   // <-- a better token (don't use this exact one, make your own)
+
+$cfg["authorized_tokens"] = array(
+    // "mysecrettoken",  // <-- not a good token
+    // "90dfs7jwn3",     // <-- a better token (don't use this exact one, make your own)
 );
 
+
+
+/////////////////////////////////////////////////////////////
+//
+// external_commands_deny_tokens
+//
 // By default, all authorized tokens are allowed to submit any
-// external command (unless it's disable below)
+// external command (unless it"s disable below)
 // This is a deny mapping in the form of COMMAND => TOKEN or TOKENS
 // You can specify a whole command, or use * as a wildcard
-// Or you can specify 'all' to stop any token from using any external command
+// Or you can specify "all" to stop any token from using any external command
 // the tokens specified can either be a string with 1 token, or an array of 1 or more tokens
-$cfg['external_commands_deny_tokens'] = array(
-//    "ACKNOWLEDGE_HOST_PROBLEM" => array("mysecrettoken", "myothertoken"),
-//    "ACKNOWLEDGE_SVC_PROBLEM" => "mysecrettoken",
-//    "all" => array("mysecrettoken", "myothertoken"),
-//    "ACKNOWLEDGE_*" => "mysecrettoken",
-//    "*_HOST_*" => array("mysecrettoken", "myothertoken"),
+
+$cfg["external_commands_deny_tokens"] = array(
+    // "ACKNOWLEDGE_HOST_PROBLEM" => array("mysecrettoken", "myothertoken"),
+    // "ACKNOWLEDGE_SVC_PROBLEM"  => "mysecrettoken",
+    // "all"                      => array("mysecrettoken", "myothertoken"),
+    // "ACKNOWLEDGE_*"            => "mysecrettoken",
+    // "*_HOST_*"                 => array("mysecrettoken", "myothertoken"),
 );
 
-    
+
+
+/////////////////////////////////////////////////////////////
+//
+// require_https
+// 
 // Do we require that HTTPS be used to access NRDP?
-// set this value to 'false' to disable HTTPS requirement
+// set this value to false to disable HTTPS requirement
+
 $cfg["require_https"] = false;
 
+
+
+/////////////////////////////////////////////////////////////
+//
+// require_basic_auth
+//
 // Do we require that basic authentication be used to access NRDP?
-// set this value to 'false' to disable basic auth requirement 
+// set this value to false to disable basic auth requirement 
+
 $cfg["require_basic_auth"] = false;
 
+
+
+/////////////////////////////////////////////////////////////
+//
+// valid_basic_auth_users
+//
 // What basic authentication users are allowed to access NRDP?
 // comment this variable out to allow all authenticated users access to the NRDP
+
 $cfg["valid_basic_auth_users"] = array(
     "nrdpuser"
 );
-    
+
+
+
+/////////////////////////////////////////////////////////////
+//
+// nagios_command_group
+//
 // The name of the system group that has write permissions to the external command file
 // this group is also used to set file permissions when writing bulk commands or passive check results
 // NOTE: both the Apache and Nagios users must be a member of this group
+
 $cfg["nagios_command_group"] = "nagcmd";
 
+
+
+/////////////////////////////////////////////////////////////
+//
+// command_file
+//
 // Full path to Nagios external command file
-$cfg["command_file"] = "/var/spool/nagios/cmd/nagios.cmd";
 
+$cfg["command_file"] = "/usr/local/nagios/var/rw/nagios.cmd";
+
+
+
+/////////////////////////////////////////////////////////////
+//
+// check_results_dir
+//
 // Full path to check results spool directory
-$cfg["check_results_dir"] = "/var/log/nagios/spool/checkresults";
 
+$cfg["check_results_dir"] = "/usr/local/nagios/var/spool/checkresults";
+
+
+
+/////////////////////////////////////////////////////////////
+//
+// disable_external_commands
+//
 // Should we allow external commands? Set to true or false (Boolean, not a string)
+
 $cfg["disable_external_commands"] = false;
 
+
+
+/////////////////////////////////////////////////////////////
+//
+// allow_old_results
+//
 // Allows Nagios XI to send old check results directly into NDO if configured
+
 $cfg["allow_old_results"] = false;
 
+
+
+/////////////////////////////////////////////////////////////
+//
+// debug
+//
 // Enable debug logging
+
 $cfg["debug"] = false;
 
+
+
+/////////////////////////////////////////////////////////////
+//
+// debug_log
+//
 // Where should the logs go?
+
 $cfg["debug_log"] = "/usr/local/nrdp/server/debug.log";
-
-
-///////// DONT MODIFY ANYTHING BELOW THIS LINE /////////
-
-$cfg['product_name'] = 'nrdp';
-$cfg['product_version'] = '1.5.2'
-
-
-?>

server/config.inc.php
@@ -1,76 +1,169 @@
 <?php
-//
-// NRDP Config File
-//
-// Copyright (c) 2010-2017 - Nagios Enterprises, LLC.
-// License: Nagios Open Software License <http://www.nagios.com/legal/licenses>
-//
+/*****************************************************************************
+ *
+ *
+ *  NRDP Configuration file
+ *
+ *
+ *  Copyright (c) 2008-2018 - Nagios Enterprises, LLC. All rights reserved.
+ *
+ *  License: GNU General Public License version 3
+ *
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ *****************************************************************************/
 
+
+
+/////////////////////////////////////////////////////////////
+//
+// authorized_tokens
+//
 // An array of one or more tokens that are valid for this NRDP install
 // a client request must contain a valid token in order for the NRDP to response or honor the request
 // NOTE: Tokens are just alphanumeric strings - make them hard to guess!
-$cfg['authorized_tokens'] = array(
-    //"mysecrettoken",  // <-- not a good token
-    //"90dfs7jwn3",   // <-- a better token (don't use this exact one, make your own)
+
+$cfg["authorized_tokens"] = array(
+    // "mysecrettoken",  // <-- not a good token
+    // "90dfs7jwn3",     // <-- a better token (don't use this exact one, make your own)
 );
 
+
+
+/////////////////////////////////////////////////////////////
+//
+// external_commands_deny_tokens
+//
 // By default, all authorized tokens are allowed to submit any
-// external command (unless it's disable below)
+// external command (unless it"s disable below)
 // This is a deny mapping in the form of COMMAND => TOKEN or TOKENS
 // You can specify a whole command, or use * as a wildcard
-// Or you can specify 'all' to stop any token from using any external command
+// Or you can specify "all" to stop any token from using any external command
 // the tokens specified can either be a string with 1 token, or an array of 1 or more tokens
-$cfg['external_commands_deny_tokens'] = array(
-//    "ACKNOWLEDGE_HOST_PROBLEM" => array("mysecrettoken", "myothertoken"),
-//    "ACKNOWLEDGE_SVC_PROBLEM" => "mysecrettoken",
-//    "all" => array("mysecrettoken", "myothertoken"),
-//    "ACKNOWLEDGE_*" => "mysecrettoken",
-//    "*_HOST_*" => array("mysecrettoken", "myothertoken"),
+
+$cfg["external_commands_deny_tokens"] = array(
+    // "ACKNOWLEDGE_HOST_PROBLEM" => array("mysecrettoken", "myothertoken"),
+    // "ACKNOWLEDGE_SVC_PROBLEM"  => "mysecrettoken",
+    // "all"                      => array("mysecrettoken", "myothertoken"),
+    // "ACKNOWLEDGE_*"            => "mysecrettoken",
+    // "*_HOST_*"                 => array("mysecrettoken", "myothertoken"),
 );
 
-    
+
+
+/////////////////////////////////////////////////////////////
+//
+// require_https
+// 
 // Do we require that HTTPS be used to access NRDP?
-// set this value to 'false' to disable HTTPS requirement
+// set this value to false to disable HTTPS requirement
+
 $cfg["require_https"] = false;
 
+
+
+/////////////////////////////////////////////////////////////
+//
+// require_basic_auth
+//
 // Do we require that basic authentication be used to access NRDP?
-// set this value to 'false' to disable basic auth requirement 
+// set this value to false to disable basic auth requirement 
+
 $cfg["require_basic_auth"] = false;
 
+
+
+/////////////////////////////////////////////////////////////
+//
+// valid_basic_auth_users
+//
 // What basic authentication users are allowed to access NRDP?
 // comment this variable out to allow all authenticated users access to the NRDP
+
 $cfg["valid_basic_auth_users"] = array(
     "nrdpuser"
 );
-    
+
+
+
+/////////////////////////////////////////////////////////////
+//
+// nagios_command_group
+//
 // The name of the system group that has write permissions to the external command file
 // this group is also used to set file permissions when writing bulk commands or passive check results
 // NOTE: both the Apache and Nagios users must be a member of this group
+
 $cfg["nagios_command_group"] = "nagcmd";
 
+
+
+/////////////////////////////////////////////////////////////
+//
+// command_file
+//
 // Full path to Nagios external command file
+
 $cfg["command_file"] = "/usr/local/nagios/var/rw/nagios.cmd";
 
+
+
+/////////////////////////////////////////////////////////////
+//
+// check_results_dir
+//
 // Full path to check results spool directory
+
 $cfg["check_results_dir"] = "/usr/local/nagios/var/spool/checkresults";
 
+
+
+/////////////////////////////////////////////////////////////
+//
+// disable_external_commands
+//
 // Should we allow external commands? Set to true or false (Boolean, not a string)
+
 $cfg["disable_external_commands"] = false;
 
+
+
+/////////////////////////////////////////////////////////////
+//
+// allow_old_results
+//
 // Allows Nagios XI to send old check results directly into NDO if configured
+
 $cfg["allow_old_results"] = false;
 
+
+
+/////////////////////////////////////////////////////////////
+//
+// debug
+//
 // Enable debug logging
+
 $cfg["debug"] = false;
 
+
+
+/////////////////////////////////////////////////////////////
+//
+// debug_log
+//
 // Where should the logs go?
+
 $cfg["debug_log"] = "/usr/local/nrdp/server/debug.log";
-
-
-///////// DONT MODIFY ANYTHING BELOW THIS LINE /////////
-
-$cfg['product_name'] = 'nrdp';
-$cfg['product_version'] = '1.5.2'
-
-
-?>

clients/send_nrdp.php
@@ -1,38 +1,35 @@
 #!/usr/bin/php -q
 <?php
-//
-// send_nrdp.php
-//
-// Copyright (c) 2010-2017 - Nagios Enterprises, LLC.
-// Portions Copyright (c) others - see source code below.
-//
-// Software License:
-//
-// Redistribution and use in source and binary forms, with or without modification,
-// are permitted provided that the following conditions are met:
-//
-// * Redistributions of source code must retain the above copyright notice, this
-//   list of conditions and the following disclaimer.
-// * Redistributions in binary form must reproduce the above copyright notice,
-//   this list of conditions and the following disclaimer in the documentation and/or
-//   other materials provided with the distribution.
-// * Neither the name of the Nagios Enterprises, LLC nor the names of its contributors
-//   may be used to endorse or promote products derived from this software without
-//   specific prior written permission.
-//
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
-// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
-// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
-// SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
-// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
-// BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
-// OF SUCH DAMAGE.
-//
+/*****************************************************************************
+ *
+ *
+ *  send_nrdp.php - Send host/service checkresults to NRDP with XML
+ *
+ *
+ *  Copyright (c) 2008-2018 - Nagios Enterprises, LLC. All rights reserved.
+ *  Portions Copyright (c) others - see source code below
+ *
+ *  License: GNU General Public License version 3
+ *
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ *****************************************************************************/
+
 // 2017-08-15 Troy Lea aka BOX293
 //  - Updated load_url function to work with https
+
 
 doit();
 
@@ -163,7 +160,7 @@ function doit()
 </checkresults>";
 
     // Build URL
-    $theurl = $url."/?token=".$token."&cmd=submitcheck&XMLDATA=".urlencode($xml);
+    $theurl = $url."/?token=".$token."&cmd=submitcheck&xml=".urlencode($xml);
 
     // Send data to NRDP
     $opts = array(

clients/send_nrdp.py
@@ -1,23 +1,49 @@
 #!/usr/bin/env python
-#
-# send_nrdp.py
-#
-# Copyright (c) 2010-2017 - Nagios Enterprises, LLC.
-# Written by: Scott Wilkerson (nagios@nagios.org)
-#
-# 2017-09-22 Troy Lea aka BOX293
-#  - Fixed setup function that was not working for piped results
-#    as the xml string was missing "</checkresults>"
-#  - Fixed setup function that was not working with arguments when
-#    run as a cron job or if being used as a nagios command like
-#    obsessive compulsive ... "if not sys.stdin.isatty():" was the
-#    reason why.
-# 2017-09-25 Troy Lea aka BOX293
-#  - Added file argument to allow XML results to be read from file
-#  - Replaced optparse with argparse so help text can include newline
-#    characters, allowing for better formatting.
-# 2017-09-26 Troy Lea aka BOX293
-#  - Made sure url ends with a / before posting
+
+##############################################################################
+ #
+ #
+ #  send_nrdp.py - Send host/service checkresults to NRDP with XML
+ #
+ #
+ #  Copyright (c) 2008-2018 - Nagios Enterprises, LLC. All rights reserved.
+ #  Originally Authored: Scott Wilkerson (nagios@nagios.org)
+ #
+ #  License: GNU General Public License version 3
+ #
+ #
+ #  This program is free software: you can redistribute it and/or modify
+ #  it under the terms of the GNU General Public License as published by
+ #  the Free Software Foundation, either version 3 of the License, or
+ #  (at your option) any later version.
+ #
+ #  This program is distributed in the hope that it will be useful,
+ #  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ #  GNU General Public License for more details.
+ #
+ #  You should have received a copy of the GNU General Public License
+ #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ #
+ #############################################################################
+ #
+ # 2017-09-22 Troy Lea aka BOX293
+ #  - Fixed setup function that was not working for piped results
+ #    as the xml string was missing "</checkresults>"
+ #  - Fixed setup function that was not working with arguments when
+ #    run as a cron job or if being used as a nagios command like
+ #    obsessive compulsive ... "if not sys.stdin.isatty():" was the
+ #    reason why.
+ #
+ # 2017-09-25 Troy Lea aka BOX293
+ #  - Added file argument to allow XML results to be read from file
+ #  - Replaced optparse with argparse so help text can include newline
+ #    characters, allowing for better formatting.
+ #
+ # 2017-09-26 Troy Lea aka BOX293
+ #  - Made sure url ends with a / before posting
+ #
+ #############################################################################
 
 
 import argparse, sys, urllib, cgi
@@ -105,7 +131,7 @@ printf \"<hostname>\\t<service>\\t<state>\\t<output>\\n\"\n")
         if not url.endswith('/'):
             url += '/'
         
-        params = urllib.urlencode({'token': token.strip(), 'cmd': 'submitcheck', 'XMLDATA': xml});
+        params = urllib.urlencode({'token': token.strip(), 'cmd': 'submitcheck', 'xml': xml});
         opener = urllib.FancyURLopener()
         try:
             f = opener.open(url, params)

clients/send_nrdp.sh
@@ -1,16 +1,42 @@
 #!/bin/bash
-#
-# check_nrdp.sh
-#
-# Copyright (c) 2010-2017 - Nagios Enterprises, LLC.
-# Written by: Scott Wilkerson (nagios@nagios.org)
-#
-# 2017-09-25 Troy Lea aka BOX293
-#  - Fixed script not working with arguments when run as a cron job
-#    or if being used as a nagios command like obsessive compulsive.
-#     ... "if [ ! -t 0 ]" was the reason why.
-# 2017-12-08 Jørgen van der Meulen (Conclusion Xforce)
-#  - Fixed typo in NRDP abbreviation
+
+##############################################################################
+ #
+ #
+ #  send_nrdp.sh - Send host/service checkresults to NRDP with XML
+ #
+ #
+ #  Copyright (c) 2008-2018 - Nagios Enterprises, LLC. All rights reserved.
+ #  Originally Authored: Scott Wilkerson (nagios@nagios.org)
+ #
+ #  License: GNU General Public License version 3
+ #
+ #
+ #  This program is free software: you can redistribute it and/or modify
+ #  it under the terms of the GNU General Public License as published by
+ #  the Free Software Foundation, either version 3 of the License, or
+ #  (at your option) any later version.
+ #
+ #  This program is distributed in the hope that it will be useful,
+ #  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ #  GNU General Public License for more details.
+ #
+ #  You should have received a copy of the GNU General Public License
+ #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ #
+ #############################################################################
+ #
+ # 2017-09-25 Troy Lea aka BOX293
+ #
+ #  - Fixed script not working with arguments when run as a cron job
+ #    or if being used as a nagios command like obsessive compulsive.
+ #     ... "if [ ! -t 0 ]" was the reason why.
+ #
+ # 2017-12-08 Jørgen van der Meulen (Conclusion Xforce)
+ #  - Fixed typo in NRDP abbreviation
+ #
+ #############################################################################
 
 
 PROGNAME=$(basename $0)
@@ -41,22 +67,22 @@ print_help() {
         echo ""
         echo "Options:"
         echo "    Single Check:"
-        echo "        -H    host name"
-        echo "        -s    service name"
+        echo "        -H    Host name"
+        echo "        -s    Service description"
         echo "        -S    State"
-        echo "        -o     output"
+        echo "        -o    Output"
         echo ""
         echo "    STDIN:"
-        echo "        [-d    delimiter] (default -d \"\\t\")"
+        echo "        -d    Delimiter (Optional, default -d \"\\t\")"
         echo "        With only the required parameters $PROGNAME is capable of"
         echo "        processing data piped to it either from a file or other"
         echo "        process.  By default, we use \t as the delimiter however this"
         echo "        may be specified with the -d option data should be in the"
         echo "        following formats one entry per line."
         echo "        For Host checks:"
-        echo "        hostname    State    output"
+        echo "        Hostname	State	Output"
         echo "        For Service checks"
-        echo "        hostname    servicename    State    output"
+        echo "        Hostname	Servicedesc	State	Output"
         echo ""
         echo "    File:"
         echo "        -f /full/path/to/file"
@@ -95,16 +121,16 @@ send_data() {
     if [ ! "x$curl" == "x" ];then
 
         if [ $file ]; then
-            fdata="--data-urlencode XMLDATA@$file"
+            fdata="--data-urlencode xml@$file"
             rslt=`curl -f --silent --insecure -d "$pdata" $fdata "$url/"`
         else
-            pdata="$pdata&XMLDATA=$1"
+            pdata="$pdata&xml=$1"
             rslt=`curl -f --silent --insecure -d "$pdata" "$url/"`
         fi
         
         ret=$?
     else
-        pdata="$pdata&XMLDATA=$1"
+        pdata="$pdata&xml=$1"
         rslt=`wget -q -O - --post-data="$pdata" "$url/"`
         ret=$?
     fi

server/includes/constants.inc.php
@@ -1,10 +1,33 @@
 <?php
-//
-// NRDP Constants
-//
-// Copyright (c) 2008-2017 - Nagios Enterprises, LLC. All rights reserved.
-// License: Nagios Open Software License <http://www.nagios.com/legal/licenses>
-//
+/*****************************************************************************
+ *
+ *
+ *  NRDP Constants
+ *
+ *
+ *  Copyright (c) 2008-2018 - Nagios Enterprises, LLC. All rights reserved.
+ *
+ *  License: GNU General Public License version 3
+ *
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ *****************************************************************************/
+
+// PRODUCT INFO
+define("PRODUCT_NAME", "nrdp");
+define("PRODUCT_VERSION", "2.0.0");
 
 // ERROR STRINGS
 define("ERROR_CAPABILITY_NOT_ENABLED","NOT ENABLED");
@@ -48,3 +71,7 @@ define("ERROR_BAD_JSON","BAD JSON");
 
 // CALLBACKS
 define("CALLBACK_PROCESS_REQUEST","PROCESS_REQUEST");
+
+// OUTPUT TYPES
+define("TYPE_XML", "TYPE_XML");
+define("TYPE_JSON", "TYPE_JSON");

commit 5fb0e983ac7cd62514e6eec8a676188390db5e55

Fix config.inc.php (broken in a675f42955e21dad33b1389bfd3dca4a852ecf07)

package/fedora/config.inc.php
@@ -167,3 +167,5 @@ $cfg["debug"] = false;
 // Where should the logs go?
 
 $cfg["debug_log"] = "/usr/local/nrdp/server/debug.log";
+
+?>

server/config.inc.php
@@ -177,3 +177,5 @@ $cfg["debug"] = false;
 // Where should the logs go?
 
 $cfg["debug_log"] = "/usr/local/nrdp/server/debug.log";
+
+?>